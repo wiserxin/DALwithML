bf7cec032a5cfaf82ecadd93a1771b3b1c05c8e3
Xin <wiserxin@163.com>
2020-05-12 03:07:35 +0800

    Update evaluator.py

--- neural/util/evaluator.py
+++ neural/util/evaluator.py
@@ -148,7 +148,7 @@ class Evaluator(object):
     # np.argpartition : O(n)
 
         (y_pred, y_true) = args
-        pred_topk_index = np.argpartition(-y_pred, self.top_k)[:self.top_k][:self.top_k] # 不分先后的选出排名靠前者
+        pred_topk_index = np.argpartition(-y_pred.data(), self.top_k)[:self.top_k][:self.top_k] # 不分先后的选出排名靠前者
         pred_topk_index = sorted(pred_topk_index, key=lambda i: y_pred[i], reverse=True) # 分先后的选出来
         print('better:',pred_topk_index)
         pos_index = set([k for k, v in enumerate(y_true) if v == 1])
