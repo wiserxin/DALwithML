a26398b3539bc5e9c11db36cbcb0a723e8f8dc19
Xin <wiserxin@163.com>
2020-05-28 23:33:41 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -2,5 +2,6 @@
 
 rm output.txt
 git pull
+CUDA_VISIBLE_DEVICES=1 python main.py --learning_rate=0.0001
 CUDA_VISIBLE_DEVICES=1 python main.py --learning_rate=0.0005
 CUDA_VISIBLE_DEVICES=1 python main.py --learning_rate=0.001
