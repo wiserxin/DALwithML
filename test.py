21478adf4bb3b7ebd427f2fe0b302f3c118f27b8
Xin <wiserxin@163.com>
2020-05-29 10:06:55 +0800

    Update test.py

--- test.py
+++ test.py
@@ -1,4 +1,4 @@
-from neural.util.loader import Loader
+from neural.util import Trainer, Loader
 from neural.models import CNN
 import torch
 from torch.autograd import Variable
@@ -22,70 +22,39 @@ def theLoss(x, target):
     return nn.MultiLabelSoftMarginLoss()(x, target)
 
 eurlex_path = r"../../datasets/eurLex/"
+rcv2_path = r"../../datasets/rcv2/"
 
 Loader = Loader()
-data = Loader.load_eurlex( datapath=eurlex_path, vocab_size=30000)
+data = Loader.load_rcv2( datapath=rcv2_path, vocab_size=30000)
 
-X_trn, Y_trn, Y_trn_o = data['train']
-X_tst, Y_tst, Y_tst_o = data['test']
+train_data = data['train_points']
+val_data = data['test_points']
 
-
-num_classes = Y_trn.shape[1]
-
+train_data = train_data[:20000]
+# too small the valdata amount so ...
+if len(val_data) < 100000:
+    val_data.extend(train_data[-5000:])
+    train_data = train_data[:-5000]
 
 model = CNN(  word_vocab_size = 30000
             , word_embedding_dim = 300
             , word_out_channels = 200
-            , output_size = num_classes
+            , output_size = 103
             , pretrained = data['embed']
             )
 model = nn.DataParallel(model).cuda()
 
-optimizer = Adam(model.parameters(), lr=0.001)
+trainer = Trainer(model,
+                  r'./result',
+                  CNN,
+                  top_k=40
+                  )
 
-evaluator = Evaluator("CNN")
-best_eval = 0.0
-now_eval  = 0.0
-
-tr_batch_size = 256
-
-for epoch in range(80):
-    torch.cuda.empty_cache()
-
-    nr_trn_num = X_trn.shape[0]
-    nr_batches = int(np.ceil(nr_trn_num / float(tr_batch_size)))
-
-
-    model.train()
-    model_loss = 0
-    for iteration, batch_idx in enumerate(np.random.permutation(range(nr_batches))):
-
-        start_idx = batch_idx * tr_batch_size
-        end_idx = min((batch_idx + 1) * tr_batch_size, nr_trn_num)
-
-        X = X_trn[start_idx:end_idx]
-        X = Variable(torch.from_numpy(X).long()).cuda()
-        Y = Y_trn[start_idx:end_idx]
-
-        batch_target = Variable(torch.from_numpy(Y.A.astype(int)).float()).cuda()
-        optimizer.zero_grad()
-        output = model(X)
-        loss = theLoss(output, batch_target)
-        loss.backward()
-        model_loss = loss.item()
-        optimizer.step()
-        torch.cuda.empty_cache()
-
-
-        print("\rEpoch: {} Iteration: {}/{} ({:.1f}%)  Loss: {:.5f} {:.5f}".format(
-                      epoch,iteration, nr_batches,
-                      iteration * 100 / nr_batches,
-                      model_loss, 0),
-                      end="")
-
-    print('')
-    if  not ( (epoch+1) % 4):
-        best_eval, now_eval, save = evaluator.evaluate(model, data['test'], best_eval, )
-        print("\rEpoch: {} Loss: {:.5f} Best_NDCG5: {:.5f} NDCG5: {:.5f}\n".format(epoch, model_loss, best_eval, now_eval))
-    torch.cuda.empty_cache()
+test_performance = trainer.train_supervisedLearning(40,
+                                                    data['train_points'],
+                                                    data['test_points'],
+                                                    0.0005,
+                                                    checkpoint_path=r'./result',
+                                                    batch_size=512
+                                                    )
 
