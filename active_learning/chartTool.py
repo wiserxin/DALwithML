a4d69d0c327a4778296673321b9f1faab2b8a7a9
Xin <wiserxin@163.com>
2020-06-09 20:55:55 +0800

    Update chartTool.py

--- active_learning/chartTool.py
+++ active_learning/chartTool.py
@@ -65,3 +65,55 @@ def updateBarChart(data):
 
     urls = '10.2.26.117/updateChart/?' + refs
     http.request('GET', urls)
+
+
+import os
+def walkFile(path):
+    txt = []
+    for root, dirs, files in os.walk(path):
+
+        # root 表示当前正在访问的文件夹路径
+        # dirs 表示该文件夹下的子目录名list
+        # files 表示该文件夹下的文件list
+
+        # 遍历文件
+        for f in files:
+            temp = os.path.abspath(os.path.join(root, f))
+            if temp[-4:] == '.txt':
+                txt.append(temp)
+    #                 print(temp)
+
+    #         遍历所有的文件夹
+    #         for d in dirs:
+    #             print(os.path.join(root, d))
+    return txt
+
+
+class VisionResult():
+    def __init__(self, filepath):
+        fileContent = []
+        with open(filepath, 'r') as f:
+            fileContent = f.readlines()
+
+        self.group_name, self.sample_method, self.round = fileContent[0][:-1].split('\t')
+        self.round = int(self.round)
+
+        self.performance = []
+        count = 0
+        while (count < self.round):
+            try:
+                self.performance.append(float(fileContent[count + 1].split('\t')[1]))
+                count += 1
+            except:
+                break
+        print("{}:{} - Round{}:{}".format(self.group_name, self.sample_method, len(self.performance), self.round))
+
+    def send(self,max_performance=0.90):
+        for i in self.performance:
+            updateLineChart(str(i), self.sample_method, gp_name=self.group_name, max=max_performance)
+
+
+if __name__ == '__main__':
+    for txt in walkFile(r'../result/'):
+        perf = VisionResult(txt)
+        perf.send(max_performance=0.90)
