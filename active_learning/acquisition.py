3ba402d8191fd3c62a45fee75d1c7b7497434729
Xin <wiserxin@163.com>
2020-08-10 15:54:34 +0800

    dsm8rkl4

--- active_learning/acquisition.py
+++ active_learning/acquisition.py
@@ -1568,6 +1568,12 @@ class Acquisition(object):
                                                       model_name=model_name, thisround=round, returned=True)
                     self.get_submodular(data, unlabeled_index, acquire_num, model_path=model_path,
                                         model_name=model_name)
+                elif sub_method == "dsm8RKL4":
+                    temp = [2, 2, 2, 2, 4, 4, 4, 4, 4, 4, 4, 4, 2, 2, 2, 2, 4,4,4,4 , 4,4,4,4, 4 ]
+                    _, unlabeled_index = self.get_RKL(data, model_path, acquire_num * temp[round],
+                                                      model_name=model_name, thisround=round, returned=True)
+                    self.get_submodular(data, unlabeled_index, acquire_num, model_path=model_path,
+                                        model_name=model_name)
 
 
 
