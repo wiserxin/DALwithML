f89a622cbaa0e906d0433cb3fe4a9560ad1665c9
Xin <wiserxin@163.com>
2020-09-01 15:15:33 +0800

    find new xmlcnn param

--- neural/models/cnn.py
+++ neural/models/cnn.py
@@ -87,7 +87,7 @@ class CNN_ori(nn.Module):
 
 
 # CNN-Kim
-class CNN(nn.Module):
+class CNN_KIM(nn.Module):
     def __init__(self, word_vocab_size, word_embedding_dim, word_out_channels, output_size,
                  dropout_p=0.5, pretrained=None, double_embedding=False, cuda_device=0):
         super(CNN, self).__init__()
@@ -156,7 +156,7 @@ class CNN(nn.Module):
         return torch.cat((x,output), 1)
 
 # XML-CNN
-class CNN_XML(nn.Module):
+class CNN(nn.Module):
     def __init__(self, word_vocab_size, word_embedding_dim, word_out_channels, output_size,
                  dropout_p=0.5, pretrained=None, double_embedding=False, cuda_device=0):
         super(CNN, self).__init__()
@@ -174,16 +174,16 @@ class CNN_XML(nn.Module):
             self.embedding.weight = nn.Parameter(torch.FloatTensor(pretrained))
 
         # CNN
-        self.conv13 = nn.Conv2d(1, word_out_channels, (4, word_embedding_dim), stride=2)
-        self.conv14 = nn.Conv2d(1, word_out_channels, (8, word_embedding_dim), stride=2)
-        self.conv15 = nn.Conv2d(1, word_out_channels, (12, word_embedding_dim), stride=2)
-        self.pool13 = nn.MaxPool1d(self.out_size(word_embedding_dim, 4, stride=2)//4, stride=16)
-        self.pool14 = nn.MaxPool1d(self.out_size(word_embedding_dim, 8, stride=2)//4, stride=16)
-        self.pool15 = nn.MaxPool1d(self.out_size(word_embedding_dim, 12, stride=2)//4, stride=16)
+        self.conv13 = nn.Conv2d(1, word_out_channels, (2, word_embedding_dim), stride=2)
+        self.conv14 = nn.Conv2d(1, word_out_channels, (4, word_embedding_dim), stride=2)
+        self.conv15 = nn.Conv2d(1, word_out_channels, (8, word_embedding_dim), stride=2)
+        self.pool13 = nn.MaxPool1d(self.out_size(word_embedding_dim, 2, stride=2)//4,)
+        self.pool14 = nn.MaxPool1d(self.out_size(word_embedding_dim, 4, stride=2)//4,)
+        self.pool15 = nn.MaxPool1d(self.out_size(word_embedding_dim, 8, stride=2)//4,)
 
         self.dropout = nn.Dropout(p=dropout_p)
 
-        hidden_size = 4400
+        hidden_size = 2400
         self.linear1 = nn.Linear(hidden_size, 512)
         self.linear2 = nn.Linear(512, output_size)
 
