5925e77795079a720c1ae6786804e80f2a231d0e
Xin <wiserxin@163.com>
2020-09-01 15:22:05 +0800

    Update cnn.py

--- neural/models/cnn.py
+++ neural/models/cnn.py
@@ -174,16 +174,16 @@ class CNN(nn.Module):
             self.embedding.weight = nn.Parameter(torch.FloatTensor(pretrained))
 
         # CNN
-        self.conv13 = nn.Conv2d(1, word_out_channels, (2, word_embedding_dim), stride=2)
-        self.conv14 = nn.Conv2d(1, word_out_channels, (4, word_embedding_dim), stride=2)
-        self.conv15 = nn.Conv2d(1, word_out_channels, (8, word_embedding_dim), stride=2)
+        self.conv13 = nn.Conv2d(1, word_out_channels, (2, word_embedding_dim), stride=1)
+        self.conv14 = nn.Conv2d(1, word_out_channels, (4, word_embedding_dim), stride=1)
+        self.conv15 = nn.Conv2d(1, word_out_channels, (8, word_embedding_dim), stride=1)
         self.pool13 = nn.MaxPool1d(self.out_size(word_embedding_dim, 2, stride=2)//4,)
         self.pool14 = nn.MaxPool1d(self.out_size(word_embedding_dim, 4, stride=2)//4,)
         self.pool15 = nn.MaxPool1d(self.out_size(word_embedding_dim, 8, stride=2)//4,)
 
         self.dropout = nn.Dropout(p=dropout_p)
 
-        hidden_size = 2400
+        hidden_size = 4800
         self.linear1 = nn.Linear(hidden_size, 512)
         self.linear2 = nn.Linear(512, output_size)
 
