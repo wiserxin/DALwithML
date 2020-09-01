dce72b7df66867db48ac8e7b26b0ef0648a41b9f
Xin <wiserxin@163.com>
2020-09-01 21:59:02 +0800

    Update test.py

--- test.py
+++ test.py
@@ -53,7 +53,7 @@ trainer = Trainer(model.module,
 test_performance = trainer.train_supervisedLearning(40,
                                                     train_data,
                                                     val_data,
-                                                    0.001,
+                                                    0.01,
                                                     checkpoint_path=r'./result',
                                                     batch_size=512,
                                                     )
