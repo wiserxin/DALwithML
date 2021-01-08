0739040f1e8a83bf92783db17de57e0805bfe119
Xin <wiserxin@163.com>
2021-01-09 00:11:21 +0800

    Update trainer.py

--- neural/util/trainer.py
+++ neural/util/trainer.py
@@ -69,11 +69,9 @@ class Trainer(object):
                 elif self._model_name in ['CNN']:
                     output = self.model(X, usecuda=self.usecuda)
 
-                print('output:',output.size(),'\n',output)
-                print(output[0])
-                print('Y:',Y.size(),'\n',Y)
-                print(Y[0])
-                assert False
+                # print('output:',output.size(),'\n',output)
+                # print('Y:',Y.size(),'\n',Y)
+                # assert False
 
                 loss = self.lossfunc(output, Y )
                 lossD += loss.item() / len(Y_o)
